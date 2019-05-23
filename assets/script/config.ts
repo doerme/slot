113ec40aad243c5c69d74c28b592238de2911788
梁晓明 <liangxiaoming@yy.com>
2019-05-23 16:47:53 +0800

    feat: 滚动调整

---
+++ assets/script/config.ts
@@ -0,0 +1,4 @@
+export default {
+  unitcolHeight: 1246, // 单列高度。一列动画由两列组成
+  unitHeight : 178 // 一格水果高度
+}
--- assets/script/scene/game.ts
+++ assets/script/scene/game.ts
@@ -1,6 +1,6 @@
 import { store } from '../store/store'
 import { observer, render, reactor, react } from "../store/observer";
-
+import CONFIG from '../config'
 const {ccclass, property} = cc._decorator;
 
 @ccclass
@@ -22,11 +22,17 @@ export default class GameScene extends cc.Component {
     // colarray
     colArray: Array<cc.Node> = []
 
+    // twocolarray 两col组成一列
+    twoColArray: Array<any> = []
+
     // 转盘初始速度
     primarySpeed: Array<number> = [0, 0, 0]
 
     // 转盘减速度
     decreaseSpeed: number = 300
+
+    // 调整速度
+    adaptSpeed: number = 300
 
     // LIFE-CYCLE CALLBACKS:
 
@@ -36,7 +42,19 @@ export default class GameScene extends cc.Component {
 
     start () {
         console.log(`game scence start`)
-        for(let index=0; index <=6; index++){
+        this.twoColArray = [{
+            state: 0, // 0 静止 1 滚动 2 调整
+            adaptOffset: 0
+        },
+        {
+            state: 0,
+            adaptOffset: 0
+        },
+        {
+            state: 0,
+            adaptOffset: 0
+        }]
+        for(let index=0; index <=5; index++){
             this.colArray.push(cc.instantiate(this.colPrefab))
         }
         this.colArray.forEach((item, index) => {
@@ -51,20 +69,52 @@ export default class GameScene extends cc.Component {
      */
     update (dt: any) {
         // console.log(`game scence update ${dt}`)
+        this._colScroll(dt)
+        this._adaptScroll(dt)
+    }
+
+    /**
+     * 开始抽奖
+     */
+    spinClick(){
+        const randomNumber = 4000 - Math.round(Math.random() * 1000)
+        this._offsetInit()
+        this.primarySpeed = [randomNumber, randomNumber, randomNumber]
+        this.twoColArray = [
+            {
+                state: 1 // 0 静止 1 滚动 2 调整
+            },
+            {
+                state: 1
+            },
+            {
+                state: 1
+            },
+        ]
+    }
+
+    /**
+     * 列滚动动画
+     */
+    _colScroll(dt: any): void {
         this.colArray.forEach((item, index) => {
+            // console.log(Math.floor(index/2), this.twoColArray, this.colArray)
+            if(this.twoColArray[Math.floor(index/2)].state != 1) {
+                return true
+            }
             const tmpOffset = (this.primarySpeed[Math.floor(index/2)])*dt
             if(tmpOffset <= 0) {
                 return true
             }
 
-                item.y -= tmpOffset
-                if(item.y < - item.height * 3/2) {
-                    if(Number(index) % 2 === 1){
-                        item.y = this.colArray[index - 1].y + item.height
-                    } else {
-                        item.y = this.colArray[index + 1].y + item.height
-                    }
+            item.y -= tmpOffset
+            if(item.y < - item.height * 3/2) {
+                if(Number(index) % 2 === 1){
+                    item.y = this.colArray[index - 1].y + item.height
+                } else {
+                    item.y = this.colArray[index + 1].y + item.height
                 }
+            }
 
             // 减速度
             if(Math.floor(index/2) === 0 || this.primarySpeed[Math.floor(index/2) - 1] <= 0){
@@ -75,15 +125,70 @@ export default class GameScene extends cc.Component {
     }
 
     /**
-     * 开始抽奖
+     * 调整滚动
      */
-    spinClick(){
-        this._offsetInit()
-
-        this.primarySpeed = [3000, 3000, 3000]
+    _adaptScroll(dt: any):void {
+        this.primarySpeed.forEach((item, index) => {
+            if(item <= 0 && this.twoColArray[index].state === 1) {
+                let offset = Number(this.colArray[index * 2].y) % CONFIG.unitHeight
+                console.log('offset',index, offset, this.colArray[index * 2].y)
+                if(offset !== 0){ // 需要卡位
+                    if(Math.abs(offset) > CONFIG.unitHeight / 3) { // 需要减 达到卡位
+                        this.twoColArray[index].adaptOffset = -(CONFIG.unitHeight - Math.abs(offset))
+                        this.twoColArray[index].state = 2
+                        if(offset > 0){
+                            this.twoColArray[index].adaptOffset = (CONFIG.unitHeight - Math.abs(offset))
+                        }else{
+                            this.twoColArray[index].adaptOffset = -(CONFIG.unitHeight - Math.abs(offset))
+                        }
+                        console.log('offset up >', index, this.twoColArray[index].adaptOffset, this.colArray[index * 2].y)
+                    } else { // 需要加达到卡位
+                        this.twoColArray[index].state = 2
+                        if(offset > 0){
+                            this.twoColArray[index].adaptOffset = -Math.abs(offset)
+                        }else{
+                            this.twoColArray[index].adaptOffset = Math.abs(offset)
+                        }
+                        console.log('offset down >', index, this.twoColArray[index].adaptOffset, this.colArray[index * 2].y)
+                    }
+                }
+            }
+        })
+        this.primarySpeed.forEach((item, index) => {
+            if(item <= 0 && this.twoColArray[index].state === 2) {
+                const offset = this.twoColArray[index].adaptOffset
+                const adaptWay = dt * this.adaptSpeed
+                if(offset > 0) { // 需要减 达到卡位
+                    if(Math.abs(offset) < adaptWay * 2) {
+                        this.colArray[index * 2].y -= offset
+                        this.colArray[index * 2 + 1].y -= offset
+                        this.twoColArray[index].state = 0
+                        this.twoColArray[index].adaptOffset = 0
+                    } else{
+                        this.colArray[index * 2].y += adaptWay
+                        this.colArray[index * 2 + 1].y += adaptWay
+                        this.twoColArray[index].adaptOffset -= adaptWay
+                    }
+                } else { // 需要加达到卡位
+                    if(Math.abs(offset) < adaptWay * 2) {
+                        this.colArray[index * 2].y += offset
+                        this.colArray[index * 2 + 1].y += offset
+                        this.twoColArray[index].state = 0
+                        this.twoColArray[index].adaptOffset = 0
+                    }else{
+                        this.colArray[index * 2].y -= adaptWay
+                        this.colArray[index * 2 + 1].y -= adaptWay
+                        this.twoColArray[index].adaptOffset += adaptWay
+                    }
+                }
+            }
+        })
     }
 
-    _offsetInit() {
+    /**
+     * 列初始偏移量设定
+     */
+    _offsetInit(): void {
         this.colArray.forEach((item, index) => {
             item.x = 220 *(Math.floor(index/2) - 1)
             if(Number(index) % 2 === 1){
@@ -92,5 +197,12 @@ export default class GameScene extends cc.Component {
                 item.y = 0
             }
         })
+    }
+
+    /**
+     * 打印列数据
+     */
+    printColData(): void {
+        console.log(this.colArray)
     }
 }
